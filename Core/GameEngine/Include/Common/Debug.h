6ee77429408b2d5a145be32f91aa13d36b0d50cc
Okladnoj <dmytrijokladnoj@gmail.com>
2026-07-10 19:11:31 +0300

    feat: implement diagnostic logging for state machines, random values, and object detection while correcting pack/unpack timing logic.

--- Core/GameEngine/Include/Common/Debug.h
+++ Core/GameEngine/Include/Common/Debug.h
@@ -292,7 +292,7 @@ public:
 // #define DEBUG_NETWORK_MAC_FLAG
 // #define DEBUG_AUTH_MAC_FLAG
 // #define DEBUG_EAC_MAC_FLAG
-// #define DEBUG_LOG_FLAG_V2
+#define DEBUG_LOG_FLAG_V2
 #define DEBUG_SETTINGS_MAC_FLAG
 
 #ifdef DEBUG_BUILDMAPCACHE_FLAG
@@ -341,5 +341,9 @@ public:
 	#define DEBUG_SETTINGS_MAC(m) MAC_LOG_TAG("DEBUG_SETTINGS_MAC", m)
 #else
 	#define DEBUG_SETTINGS_MAC(m) ((void)0)
+#endif
+
+#ifdef DEBUG_LOG_FLAG_V2
+	#define DEBUG_LOG(m) MAC_LOG_TAG("DEBUG_LOG_V2", m)
 #endif
 
